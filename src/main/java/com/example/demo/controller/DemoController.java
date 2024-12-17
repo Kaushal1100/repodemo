23ecfe5670425a7d55d0948b05c28571e2d4150c
njoshi <njoshi@DESKTOP-0A763D8.attlocal.net>
2024-12-17 12:26:50 -0500

    GitSpringDemo: Initial Commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+		
+		@GetMapping("/dummyData")
+		public String getDummyData() {
+			return "Lets return some string";
+		}
+
+	}
+
+
